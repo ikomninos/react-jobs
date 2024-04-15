57cf5b3b025d225118e4beacb192f779d78fec06
ikomninos <76184199+ikomninos@users.noreply.github.com>
2024-04-15 22:54:56 +0300

    first commit

---
+++ src/pages/JobsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import JobListings from '../components/JobListings'
+
+
+const JobsPage = () => {
+  return (
+  <section className='bg-blu-50 px-4 py-6'> <JobListings/> </section>
+  )
+}
+
+export default JobsPage
